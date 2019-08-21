bedf095ee0f196393e2e8193e3733af0f8dbd482
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-08-21 23:44:46 +0300

    v.2.0

--- script.js
+++ script.js
@@ -25,9 +25,8 @@ function clock() {
   let hours = date.getHours();
   hours < 10 ? hours = `0${hours}` : hours;
 
-  document.getElementById('clock').innerHTML = `${hours}:${minutes}:${seconds}`;
+  return document.getElementById('clock').innerHTML = `${hours}:${minutes}:${seconds}`;
 };
-
 
 setInterval(clock);
 
@@ -53,26 +52,43 @@ document.getElementById('stop--timer').onclick = function(){
   return audio.pause();
 };
 
-function getAlarm(){
+function getMSecToAlarm (time, id){
 
-  let date = new Date();
-  let minute = date.getMinutes();
-  let hour = date.getHours();
+  const arrTime = time.split(':');
 
-  let check = document.getElementById('alarmOn');
-  let getHour = document.getElementById('hour').value;
-  let getMinute = document.getElementById('minute').value;
+  const hourNow = +arrTime[0];
+  const minuteNow = +arrTime[1];
+  const secondNow = +arrTime[2];
 
-  if (check.checked && (+getHour === +hour && +getMinute === +minute)) {
-    audio.play();
-  };
+  const getAlarmTime = document.getElementById(id).value.split(':');
+
+  const alarmHour = +getAlarmTime[0];
+  var alarmMinute = +getAlarmTime[1];
+
+  if (isNaN(alarmMinute)){
+    alarmMinute = 0;
+  }
+
+  const dayInMsec = 24 * 60 * 60 * 1000;
+
+  var timeNowInMSec = hourNow * 3600000 + minuteNow * 60000 + secondNow * 1000;
+
+  if (hourNow > alarmHour || hourNow === alarmHour && minuteNow >= alarmMinute){
+
+    return dayInMsec - timeNowInMSec + alarmHour * 3600000 + alarmMinute * 60000;
+
+  } else {
+
+    return (alarmHour * 3600000 + alarmMinute * 60000) - timeNowInMSec;
+  }
 };
 
-document.getElementById('alarmOn').onclick = function(){
-  setInterval(getAlarm, 1000);
-};
+document.getElementById('alarm--on').onclick = function(){
 
-document.getElementById('stop--alarm').onclick = function(){
-  return audio.pause();
+  timerId = setTimeout (() => audio.play(), getMSecToAlarm (clock(), 'alarm--time'))
 }
-  
+
+document.getElementById('alarm--off').onclick = function(){
+  audio.pause();
+  clearTimeout(timerId);
+};
